18b0230c065ac45495f629ea627a7c17052b7cc5
tanjimsourov <tanzim0627@icloud.com>
2024-10-30 23:15:12 +0600

    Blockchain Initialized

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 30 15:47:24 2024
+#Create a blockchain
 
-@author: spicy
-"""
+#importing libraries
 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#building a blockchain
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+        
+        
+#Mining Blockchain
